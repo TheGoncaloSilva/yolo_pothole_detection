6d3d0a7a150347b2a0944e1745cf03ddb481d75d
Gonçalo Silva <goncalolsilva@ua.pt>
2024-11-20 13:53:40 +0000

    Remove unused parameters from model training function in training script

--- training_script.py
+++ training_script.py
@@ -116,9 +116,6 @@ if __name__ == '__main__':
     model.train(data=args.datafile,
                 epochs=args.epochs,
                 imgsz=(image_info[0], image_info[1], image_info[2]),
-                seed=42,
-                batch=2,
-                workers=4,
                 patience=10)  # Early stopping if no improvement after 10 epochs
     logging.info("Model has finished training")
     # Evaluate the model
